530620e398fb7f1aea2e856958b44fdcda7aa27a
Roshni <roshni.cse.01@gmail.com>
2021-01-19 02:15:25 +0530

    Checkpoint 5

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,26 +9,18 @@ public class Main {
 
         Flight f=new Flight("123","AI",128,33);
         Passenger p=new Passenger("71","Bn", "UP","Ay","900000000","ayus@gmail.com",12);
-//        Ticket ticket=new Ticket("2234556","BN","DL", LocalDateTime.now(),LocalDateTime.now().plusMinutes(70),p,"33",
-//                false,675,f);
-//        RegularTicket r=new RegularTicket("Food");
-//        TouristTicket t=new TouristTicket("BNww",locations);
 
-        System.out.println(f.checkFlightDetails());
-        System.out.println(f.checkSeatAvailability());
+       RegularTicket regular=new RegularTicket("R2234556","BN","DL", LocalDateTime.now(),
+               LocalDateTime.now().plusMinutes(70),p,"33", false,675,f,"Food");
+        TouristTicket tourist=new TouristTicket("T2234556","BN","DL", LocalDateTime.now(),LocalDateTime.now().plusMinutes(70),
+                p,"33", false,675,f,"BNww",locations);
 
-        System.out.println(f.getAirlineName());
+        printTicketDetails(regular);
+        printTicketDetails(tourist);
 
-//        System.out.println(ticket.checkStatus());
-//        System.out.println(ticket.getDuration());
-//
-//
-//        System.out.println(f);
-//        System.out.println(p);
-//        System.out.println(ticket);
-//        System.out.println(r);
-//        System.out.println(t);
-
+    }
+    public static void printTicketDetails(Ticket ticket){
+        System.out.println(ticket.getPNRNumber());
 
     }
 }
